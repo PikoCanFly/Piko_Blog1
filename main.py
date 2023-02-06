83b291a6086ba680247fd2dbeaf3e669e37139aa
PikoCanFly <pikopiko.hakopako@gmail.com>
2023-02-07 00:12:13 +0100

    deleted import email_validator

--- main.py
+++ main.py
@@ -14,11 +14,12 @@ from functools import wraps
 from flask import abort
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+import os
 
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -27,7 +28,7 @@ gravatar = Gravatar(app,
                     size=100,
                     rating='g')
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
